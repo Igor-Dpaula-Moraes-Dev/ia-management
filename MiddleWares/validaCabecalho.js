194eef9e29b968a8cdd63ffbf9415b3d8171787d
Igor Moraes <igormoraesreis@hotmail.com>
2024-12-04 12:15:37 -0300

    Atualização de middleware constando  openai-beta=assistant =v2

--- MiddleWares/validaCabecalho.js
+++ MiddleWares/validaCabecalho.js
@@ -2,9 +2,15 @@
 export default function verificaHeader(req,res,next){
 
     const authorization = req.headers.authorization;
+    const openaiBeta = req.headers["openai-beta"];//captura o cabeçalho Openai-Beta
 
 if (!authorization || !authorization.startsWith("Bearer ")) {
   return res.status(400).json({ error: "API Key não fornecida ou inválida no cabeçalho." });
+}
+
+//verificando se o cabeçalho OpenAi-Beta está presente e tem o valor esperado
+if(!openaiBeta || openaiBeta !== "assistant=v2"){
+    return res.status(400).json({error:"Cabeçalho openAI-Beta: assistant=v2 é obrigatorio"})
 }
     const apiKey = authorization.split(" ")[1]; // Extrai a chave após "Bearer"
     req.apiKey = apiKey;
